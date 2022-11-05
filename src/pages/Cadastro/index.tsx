96b53dae38b17218a484234ad4cdb8953b7e6f5e
lsrosa1 <lucassantos.rosa@philips.com>
2022-11-05 10:50:23 -0300

    cria formulário de cadastro de cliente

--- src/pages/Cadastro/index.tsx
+++ src/pages/Cadastro/index.tsx
@@ -1,5 +1,47 @@
+import React, { useState } from 'react';
+import DatePicker from "react-datepicker";
+import Calendar from 'react-calendar';
+import Datetime from 'react-datetime';
+import "react-datepicker/dist/react-datepicker.css";
+import 'react-calendar/dist/Calendar.css';
+import "react-datetime/css/react-datetime.css";
+//import 'react-datepicker/dist/react-datepicker-cssmodules.css';
+
 export const Cadastro = () => {
+    //const [startDate, setStartDate] = useState(new Date());
+    const [value, onChange] = useState(new Date());
+    
     return (
-        <div>Cadastro</div>
+        <div>
+            <div>Cadastro</div>
+
+            <div>
+                <form>
+                    <div>
+                        <label>
+                            Name: <input type="text" name="name" />
+                        </label>
+                        <label>
+                            LastName: <input type="text" name="lastname" />
+                        </label>
+                    </div>
+                    <div>
+                        <label>
+                            CPF: <input type="text" name="cpf" />
+                        </label>
+                    </div>
+                    <div>
+                        <label>
+                            Birthdate: <Datetime />
+                        </label>
+                    </div>
+                    <div>
+                        <label>
+                            Endereços: <input type="text" name="birthdate" />
+                        </label>
+                    </div>
+                </form>
+            </div>
+        </div>
     );
 }
