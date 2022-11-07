18fe2426fa3497ad4fd7b03460f48da2a58de344
Eduardo Suzigan <eduardo.suzigan@philips.com>
2022-11-07 08:14:05 -0300

    Ajustes codigo limpo

--- src/contexts/Auth/AuthProvider.tsx
+++ src/contexts/Auth/AuthProvider.tsx
@@ -6,19 +6,7 @@ import { AuthContext } from "./AuthContext"
 export const AuthProvider = ({children}: {children: JSX.Element}) => {
     const [user, setUser] = useState<User | null>(null);
     const api = useApi();
-
-    // useEffect(() => {
-    //     const validateToken = async () => {
-    //         const storageData = localStorage.getItem('authToken');
-    //         if(storageData) {
-    //             const data = await api.validateToken(storageData);
-    //             if (data.user) {
-    //                 setUser(data.user);
-    //             }
-    //         }
-    //     }
-    //     validateToken();
-    // }, [api]);
+    
     const login = async (login: string, password: string) => {
         return api.login(login, password).then((data) => {
             console.log(data);
@@ -31,11 +19,6 @@ export const AuthProvider = ({children}: {children: JSX.Element}) => {
         }).finally(() => {
             return false;
         })
-        // if (data.user && data.token) {
-        //     setUser(data.user);
-        //     setToken(data.token);
-        //     return true;
-        // }
     }
 
     const logout = async () => {
